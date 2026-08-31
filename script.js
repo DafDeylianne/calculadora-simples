b5cc57035036af493b80486458b45417e5f0669a
DafDeylianne <ddeylianne@gmail.com>
2026-08-31 16:17:18 -0300

    feat: atualiza funcionalidades da calculadora

--- script.js
+++ script.js
@@ -6,6 +6,79 @@ botoesNumeros.forEach(function(botao) {
 
     botao.addEventListener("click", function() {
 
+        display.value = display.value + botao.textContent;
+
     });
+
+});
+
+
+const botaoLimpar = document.querySelector(".btn-limpar");
+
+botaoLimpar.addEventListener("click", function() {
+
+    display.value = "";
+
+});
+
+
+const botoesOperacao = document.querySelectorAll(".btn-operacao");
+
+let primeiroNumero;
+let operacaoAtual;
+
+botoesOperacao.forEach(function(botao) {
+
+    botao.addEventListener("click", function() {
+
+        primeiroNumero = display.value;
+
+        operacaoAtual = botao.textContent;
+
+        display.value = "";
+
+    });
+
+});
+
+
+const botaoIgual = document.querySelector(".btn-igual");
+
+botaoIgual.addEventListener("click", function() {
+
+    let segundoNumero;
+
+    segundoNumero = display.value;
+
+    segundoNumero = Number(segundoNumero);
+    primeiroNumero = Number(primeiroNumero);
+
+    if (operacaoAtual === "+") {
+
+        display.value = primeiroNumero + segundoNumero;
+
+    } else if (operacaoAtual === "-") {
+
+        display.value = primeiroNumero - segundoNumero;
+
+    } else if (operacaoAtual === "X") {
+
+        display.value = primeiroNumero * segundoNumero;
+
+    } else if (operacaoAtual === "/") {
+
+        display.value = primeiroNumero / segundoNumero;
+
+    }
+
+});
+
+const botaoDeletarTudo = document.querySelector(".btn-deletar-tudo");
+
+botaoDeletarTudo.addEventListener("click", function() {
+
+    display.value = "";
+    primeiroNumero = "";
+    operacaoAtual = "";
 
 });
